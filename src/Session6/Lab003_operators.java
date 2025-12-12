270a9dd5816b6d36f0f8c001f038ef308163c914
Rashi Jain <rashi.jain@technoduxx.com>
2025-12-12 17:40:47 +0530

    First Commit

---
+++ src/Session6/Lab003_operators.java
@@ -0,0 +1,15 @@
+package Session6;
+
+// Arithmetic operators
+//   Assignment operators
+//   Modulus operators %
+
+public class Lab003_operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        String name = "Rashi";
+        System.out.println(a+b);
+        System.out.println(a+b+name);
+    }
+}
