aa0b8270cf79ac0e55b0579f8e421b2b6154bbcb
Rashi Jain <rashi.jain@technoduxx.com>
2026-01-05 17:36:31 +0530

    for loop program

---
+++ src/Session10/Lab114_TableProgram.java
@@ -0,0 +1,11 @@
+package Session10;
+
+public class Lab114_TableProgram {
+    public static void main(String[] args) {
+        int num = 18;
+        for(int i=1; i<11;i++){
+            System.out.println(num+" "+"X"+ " "+i+" "+ "="+" "+num*i);
+
+        }
+    }
+}
